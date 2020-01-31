4369305e729eb24a1fe0e23cf2b1bc4bda4023d0
Evan Heidtmann <evan.heidtmann@gmail.com>
2020-01-30 19:47:14 -0800

    Initial Commit

---
+++ Burro/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Burro
+//
+//  Created by Evan Heidtmann on 1/30/20.
+//  Copyright © 2020 Evan Heidtmann. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
